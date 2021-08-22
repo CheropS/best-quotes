e572678d372c121b392b2d2beb5efbddec7f7534
Sharon Cherop <sh.cherop@gmail.com>
2021-08-22 18:42:23 +0300

    adding description

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,11 +10,11 @@ import { Quotes } from '../quotes';
 
 export class QuoteComponent implements OnInit {
   quotes:Quotes[]=[
-    {author:'Oscar Wilde', quote:'Be yourself, everyone else is taken'},
-    {author: 'Mahtma Gandhi', quote: 'Be the change that you wish to see in the world'},
-    {author: 'Frank Zappa', quote: 'So many books, so little time'},
-    {author: 'Albert Einstein', quote:'Two things are infinite-the universe and human stupidity, and I am not sure about the universe.'},
-    {author: 'Dr Seuss', quote: 'You know you are in love when you cannot fall asleep because reality is finally better than your dreams.'},
+    {author:'Oscar Wilde', quote:'Be yourself, everyone else is taken', description:'It is great to be yourself'},
+    {author: 'Mahtma Gandhi', quote: 'Be the change that you wish to see in the world', description:'change is good'},
+    {author: 'Frank Zappa', quote: 'So many books, so little time', description:'books are life'},
+    {author: 'Albert Einstein', quote:'Two things are infinite-the universe and human stupidity, and I am not sure about the universe.', description:'universe and stupidity'},
+    {author: 'Dr Seuss', quote: 'You know you are in love when you cannot fall asleep because reality is finally better than your dreams.', description:'sleep and reality'},
   ];
 
   addNewQuote(quote:any) {
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,7 @@
 export class Quotes {
     author!: string;
     quote!: string;
+    description!:string;
 
    
 }
