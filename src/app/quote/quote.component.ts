23143d3259ab2d546d05ce7a14bff6c4552d3ea9
Sharon Cherop <sh.cherop@gmail.com>
2021-08-22 13:55:06 +0300

    Editing quote cards on html and css

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,11 +9,11 @@ import { Quotes } from '../quotes';
 
 export class QuoteComponent implements OnInit {
   quotes:Quotes[]=[
-    {author:'Oscar Wilde', quote:'Be yourself, everyone else is taken', title:'Self-Love'},
-    {author: 'Mahtma Gandhi', quote: 'Be the change that you wish to see in the world', title:'Change'},
-    {author: 'Frank Zappa', quote: 'So many books, so little time', title:'Books'},
-    {author: 'Albert Einstein', quote:'Two things are infinite-the universe and human stupidity, and I am not sure about the universe.', title:'Humans'},
-    {author: 'Dr Seuss', quote: 'You know you are in love when you cannot fall asleep because reality is finally better than your dreams.', title:'Love'},
+    {author:'Oscar Wilde', quote:'Be yourself, everyone else is taken'},
+    {author: 'Mahtma Gandhi', quote: 'Be the change that you wish to see in the world'},
+    {author: 'Frank Zappa', quote: 'So many books, so little time'},
+    {author: 'Albert Einstein', quote:'Two things are infinite-the universe and human stupidity, and I am not sure about the universe.'},
+    {author: 'Dr Seuss', quote: 'You know you are in love when you cannot fall asleep because reality is finally better than your dreams.'},
   ]
 
   constructor() { }
